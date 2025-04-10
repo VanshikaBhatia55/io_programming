f90b01f1bc705ff1d4ee744aaf70058eef2d27c3
vanshika <vanshika2260@chitkara.edu.in>
2025-04-10 18:51:02 +0530

    [Vanshika] Add. Validating email using regex.

---
+++ json_data/src/main/java/validate_email/ValidateEmail.java
@@ -0,0 +1,13 @@
+package validate_email;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateEmail {
+	public static boolean isValid(String email) {
+		String regex = "^[a-z0-9_.-]+@[a-z0-9_.-]+$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+}
